d0e26c75944ec1369a91363e53ac848ba10a73c0
Muhammad Obaidullah <obaidlgs2005@gmail.com>
2026-01-22 15:33:02 +0500

    Data Ingestion Pipeline

---
+++ src/__init__.py
@@ -0,0 +1,4 @@
+#Whenever the find packages is running, it will come to the src folder and see how many folder you 
+# have this underscore py, and it directly considers the source as a package.
+
+
